03c995d163b2c2caa7c870d25b4cd9d2ab1b4e97
Can Nahum <cannahum@outlook.com>
2020-03-08 14:12:31 -0700

    Step 1 finished.

---
+++ 034_photo-blog/00_my-photo-blog/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	fmt.Println("listening at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
